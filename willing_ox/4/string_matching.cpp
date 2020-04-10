b74edfff61bd74f6aba17f1bc229cb5de17cd2e4
kipp <kadima.kipp@gmail.com>
2020-04-10 21:09:11 +0800

    add string

---
+++ willing_ox/4/string_matching.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+int BF(std::string &s,std::string &t)
+{
+    return 0;
+}
+
+int KMP(std::string &s, std::string &t)
+{
+    return 0;
+}
+
+int main()
+{
+    return 0;
+}
--- willing_ox/readme.md
+++ willing_ox/readme.md
@@ -4,3 +4,4 @@
 1. [线性表](1/readme.md)
 2. [栈与队列](2/readme.md)
 3. [数组](3/readme.md)
+4. [字符串](4/readme.md)
